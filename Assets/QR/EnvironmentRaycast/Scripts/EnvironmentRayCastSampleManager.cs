6027ba0247628b104c16137d4c03cf15d38e8c5b
Ahmad Baki <35337080+ahmad-baki@users.noreply.github.com>
2025-05-20 13:06:50 +0200

    - added QR-Tracking in Scene

--- Assets/QR/EnvironmentRaycast/Scripts/EnvironmentRayCastSampleManager.cs
+++ Assets/QR/EnvironmentRaycast/Scripts/EnvironmentRayCastSampleManager.cs
@@ -8,50 +8,47 @@ using UnityEngine;
 using UnityEngine.Android;
 #endif
 
-namespace PassthroughCameraSamples.MultiObjectDetection
+[MetaCodeSample("PassthroughCameraApiSamples-MultiObjectDetection")]
+public class EnvironmentRayCastSampleManager : MonoBehaviour
 {
-    [MetaCodeSample("PassthroughCameraApiSamples-MultiObjectDetection")]
-    public class EnvironmentRayCastSampleManager : MonoBehaviour
+    private const string SPATIALPERMISSION = "com.oculus.permission.USE_SCENE";
+    [SerializeField] private EnvironmentRaycastManager m_raycastManager;
+
+    private void Start()
     {
-        private const string SPATIALPERMISSION = "com.oculus.permission.USE_SCENE";
-        [SerializeField] private EnvironmentRaycastManager m_raycastManager;
-
-        private void Start()
+        if (!EnvironmentRaycastManager.IsSupported)
         {
-            if (!EnvironmentRaycastManager.IsSupported)
-            {
-                Debug.LogError("EnvironmentRaycastManager is not supported: please read the official documentation to get more details. (https://developers.meta.com/horizon/documentation/unity/unity-depthapi-overview/)");
-            }
+            Debug.LogError("EnvironmentRaycastManager is not supported: please read the official documentation to get more details. (https://developers.meta.com/horizon/documentation/unity/unity-depthapi-overview/)");
         }
+    }
 
-        public bool HasScenePermission()
-        {
+    public bool HasScenePermission()
+    {
 #if UNITY_ANDROID
-            return Permission.HasUserAuthorizedPermission(SPATIALPERMISSION);
+        return Permission.HasUserAuthorizedPermission(SPATIALPERMISSION);
 #else
-            return true;
+        return true;
 #endif
-        }
+    }
 
-        public Vector3? PlaceGameObjectByScreenPos(Ray ray)
+    public Vector3? PlaceGameObjectByScreenPos(Ray ray)
+    {
+        if (EnvironmentRaycastManager.IsSupported)
         {
-            if (EnvironmentRaycastManager.IsSupported)
+            if (m_raycastManager.Raycast(ray, out var hitInfo))
             {
-                if (m_raycastManager.Raycast(ray, out var hitInfo))
-                {
-                    return hitInfo.point;
-                }
-                else
-                {
-                    Debug.Log("RaycastManager failed");
-                    return null;
-                }
+                return hitInfo.point;
             }
             else
             {
-                Debug.LogError("EnvironmentRaycastManager is not supported");
+                Debug.Log("RaycastManager failed");
                 return null;
             }
+        }
+        else
+        {
+            Debug.LogError("EnvironmentRaycastManager is not supported");
+            return null;
         }
     }
 }
